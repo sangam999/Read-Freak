b8d9de53a84ca166361a901b87b1769e9a51b41a
Sangam <sangamsrivastav2000@gmail.com>
2024-03-18 17:52:37 +0530

    changes in files

--- src/services/createUserservices.ts
+++ src/services/createUserservices.ts
@@ -1,51 +1,39 @@
 import bcrypt from "bcrypt";
 import IUser from "../interfaces/IUser";
 import UserSchema from "../model/schema/userSchema";
-import userSchema from "../model/schema/userSchema";
-import userSchemaModel from "../model/schema/userSchema";
-import userResponse from "../api/response/userResponse";
-import * as string_decoder from "string_decoder";
-import {HydratedDocument, QueryWithHelpers} from "mongoose";
-
+import { Response } from "express";
 
 export class createUserservice {
-static async createUserservice(username: string, password: string, email: string): Promise<IUser> {
+    static async createUserservice(username: string, password: string, email: string, res: Response): Promise<IUser> {
+        try {
+            // Check if the username or email is already taken
+            const existingUsername = await UserSchema.findOne({ username: username });
+            if (existingUsername) {
+                throw new Error("Username is already taken");
+            }
 
-        const ifUsernameData: IUser[] = await UserSchema.find({username: username});
-        const ifEmailData: IUser[] = await UserSchema.find({username: username})
-        // Check if the username or email is already taken
-        if (ifUsernameData.length) {
-            throw new Error("Username is already taken");
+            const existingEmail = await UserSchema.findOne({ email: email });
+            if (existingEmail) {
+                throw new Error("Email is already registered");
+            }
+
+            // Hash the password before saving it
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Create a new user object
+            const newUser: { password: string; name: string; email: string } = {
+                name: username,
+                password: hashedPassword,
+                email: email
+            };
+
+            // Save the user to the database
+            const createdUser = await UserSchema.create(newUser);
+
+            return createdUser;
+        } catch (error) {
+            console.error("Error creating user:", error);
+            throw new Error("Error creating user");
         }
-        if (ifEmailData.length) {
-            throw new Error("Email is already registered");
-        }
-
-        // Hash the password before saving it
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create a new user object
-        let user: IUser
-       = <IUser>{
-            name: String(UserSchema.length + 1),
-            password: hashedPassword,
-        };
-
-        // Save the user to the list of users
-        userSchemaModel.insertMany([user]);
-
-        return user;
-    }
-
-    static getUserById(id: string): QueryWithHelpers<Array<HydratedDocument<IUser, {}, {}>>, HydratedDocument<IUser, {}, {}>, {}, IUser, "find"> {
-        return UserSchema.find((user: { id: string; }) => user.id === id);
-    }
-
-    static getUserByUsername(username: string):QueryWithHelpers<Array<HydratedDocument<IUser, {}, {}>>, HydratedDocument<IUser, {}, {}>, {}, IUser, "find">{
-        return userSchema.find((user: { username: string; })=> user.username === username);
-    }
-
-    static getUserByEmail(email: string): QueryWithHelpers<Array<HydratedDocument<IUser, {}, {}>>, HydratedDocument<IUser, {}, {}>, {}, IUser, "find"> {
-        return userSchema.find((user: { email: string; }) => user.email === email);
     }
 }
